87c071b01396af1bdfefe9d51abd21fa4cd35593
Mnkith <munkithabid@gmail.com>
2020-10-27 04:04:42 -0500

    commit tree 86a1cea569a6c61e74cf9cf047456ac6b92f21bb

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min_key = nil
+  min = 0
+  name_hash.each { |name, value|  min = value; break} # capturing the first value
+  name_hash.each  do |name, value| 
+    if value <= min 
+      min = value 
+      min_key = name
+    end
+  end
+  min_key
 end
